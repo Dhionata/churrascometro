d83ee0fcae3862c46bf4d72d318e103b6b50cc4e
Dhionatã <xiyunagi@gmail.com>
2021-11-22 21:49:54 -0300

    1a versão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'churrascometro';
+
+  qtdHomens: number = 0;
+  qtdMulheres: number = 0;
+  qtdPicanhaKg: number = 0;
+  refriGarrafa2Lt: number = 0;
+
+  calcular(): void {
+    this.qtdPicanhaKg = 0.350 * this.qtdHomens + 0.250 * this.qtdMulheres;
+    this.refriGarrafa2Lt = Math.ceil((0.500 * this.qtdHomens + 0.500 * this.qtdMulheres) / 2);
+  }
 }
